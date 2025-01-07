c3da7a2d0de9ea54b37ea6e78c4fb7a25ddc1a81
TheoDeschamps <theo.deschamps@epitech.eu>
2025-01-07 12:04:39 +0100

    [ADD] Step 2 to 9, socket.io doc

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM node:22.12.0
 WORKDIR /app
 COPY package*.json ./
-RUN  npm install
+RUN npm install
+RUN npm install -g tsx@4.19.2
 COPY . .
 EXPOSE 3000
 CMD ["npx", "tsx", "src/index.ts"]
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,92 @@
 import express from 'express';
-import { createServer } from 'node:http';
-import { fileURLToPath } from 'node:url';
-import { dirname, join } from 'node:path';
+import {createServer} from 'node:http';
+import {fileURLToPath} from 'node:url';
+import {dirname, join} from 'node:path';
+import {Server} from 'socket.io';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import { availableParallelism } from 'node:os';
+import cluster from 'node:cluster';
+import { createAdapter, setupPrimary } from '@socket.io/cluster-adapter';
 
-const app = express();
-const server = createServer(app);
+// open the database file
+const db = await open({
+    filename: 'chat.db',
+    driver: sqlite3.Database
+});
 
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS messages (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      client_offset TEXT UNIQUE,
+      content TEXT
+  );
+`);
+if (cluster.isPrimary) {
+    const numCPUs = availableParallelism();
+    // create one worker per available core
+    for (let i = 0; i < numCPUs; i++) {
+        cluster.fork({
+            PORT: 3000 + i
+        });
+    }
+
+    // set up the adapter on the primary thread
+    setupPrimary();
+} else {
+    const app = express();
+    const server = createServer(app);
+    const io = new Server(server, {
+        connectionStateRecovery: {},
+        // set up the adapter on each worker thread
+        adapter: createAdapter()
+    });
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 app.get('/', (req, res) => {
     res.sendFile(join(__dirname, '../public/index.html'));
 });
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000');
+io.on('connection', async (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', async (msg, clientOffset, callback) => {
+        console.log('message: ' + msg);
+        let result;
+        try {
+            result = await db.run('INSERT INTO messages (content, client_offset) VALUES (?, ?)', msg, clientOffset);
+        } catch (e) {
+            // @ts-ignore
+            if (e.errno === 19 /* SQLITE_CONSTRAINT */ ) {
+                callback();
+            } else {
+                // nothing to do, just let the client retry
+            }
+            return;
+        }
+        io.emit('chat message', msg, result.lastID);
+        callback();
+    });
+    if (!socket.recovered) {
+        // if the connection state recovery was not successful
+        try {
+            await db.each('SELECT id, content FROM messages WHERE id > ?',
+                [socket.handshake.auth.serverOffset || 0],
+                (_err, row) => {
+                    socket.emit('chat message', row.content, row.id);
+                }
+            )
+        } catch (e) {
+            // something went wrong
+        }
+    }
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
 });
+
+    const port = process.env.PORT;
+
+    server.listen(port, () => {
+        console.log(`server running at http://localhost:${port}`);
+    });
+}
