1f8b37b5f132675bc3bb63115705146c0e1b3ec1
TheoDeschamps <theo.deschamps@epitech.eu>
2025-01-21 15:58:54 +0100

    [FIX] remove dist

--- dist/models/channel.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-// Schéma pour les channels
-const channelSchema = new Schema({
-    name: { type: String, required: true, unique: true },
-    createdAt: { type: Date, default: Date.now },
-});
-// Modèle Mongoose
-const Channel = mongoose.models.Channel || mongoose.model('Channel', channelSchema);
-export default Channel;
--- dist/models/nickname.js
+++
@@ -1,9 +0,0 @@
-import mongoose, { Schema } from 'mongoose';
-// Schéma pour les nicknames
-const nicknameSchema = new Schema({
-    socketId: { type: String, required: true, unique: true },
-    nickname: { type: String, required: true },
-});
-// Modèle Mongoose
-const Nickname = mongoose.models.Nickname || mongoose.model('Nickname', nicknameSchema);
-export default Nickname;
